d57ecba33c9134d8947ff63e296ae14bb5b4c0a9
Your Name <you@example.com>
2022-01-05 06:03:57 -0800

    restructured the dir

---
+++ elpa/goto-chg-20211230.1101/goto-chg-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from goto-chg.el  -*- no-byte-compile: t -*-
+(define-package "goto-chg" "20211230.1101" "Go to last change" '((emacs "24.1")) :commit "8d45a621bf7e99f56a8bc7c526bf6c413520b464" :authors '(("David Andersson <l.david.andersson(at)sverige.nu>")) :maintainer '("Vasilij Schneidermann" . "mail@vasilij.de") :keywords '("convenience" "matching") :url "https://github.com/emacs-evil/goto-chg")
